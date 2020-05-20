5244d4d10da66c0513265ddc13b8f075fd89d695
Chad Rutherford <chad_rutherford@outlook.com>
2020-05-20 15:35:51 -0400

    Finished single_number.py

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,14 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    for i in range(len(arr)):
+        twice_flag = False
+        for j in range(len(arr)):
+            if i != j and arr[i] == arr[j]:
+                twice_flag = True
+        if not twice_flag:
+            return arr[i]
+    return -1
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
