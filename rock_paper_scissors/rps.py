b6ecea6a8319fd5a67128314acb83427171e3af0
Elisabeth <elisabeth@github.com>
2020-06-09 09:50:00 -1000

    Rock, paper, scissors.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,16 +1,32 @@
-#!/usr/bin/python
+"""
+Algorithms, stretch problem 1
+"""
 
 import sys
 
 def rock_paper_scissors(n):
-  # Your code here
+    """
+    Returns all possible sequences of rock, paper, scissors moves in a game of
+    length n.
+    """
+    if n == 0:
+        return [[]]
+    if n == 1:
+        return [['rock'], ['paper'], ['scissors']]
 
-  pass
+    plays = rock_paper_scissors(1)
+    for _ in range(n - 1):
+        new_plays = []
+        for move_list in plays:
+            for move in rock_paper_scissors(1):
+                new_plays += [move_list + move]
+        plays = new_plays
+    return plays
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
