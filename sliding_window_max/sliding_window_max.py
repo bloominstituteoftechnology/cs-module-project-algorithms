bfb903c033413b1d673cca78d77dbf66b85d931b
Elisabeth <elisabeth@github.com>
2020-06-09 09:27:57 -1000

    Sliding window max - too slow for large input.

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,16 +1,24 @@
-'''
-Input: a List of integers as well as an integer `k` representing the size of the sliding window
-Returns: a List of integers
-'''
-def sliding_window_max(nums, k):
-    # Your code here
+"""
+Algorithms, problem 4
+"""
 
-    pass
+def sliding_window_max(nums, k):
+    """
+    Input: a list of integers as well as an integer `k` representing the size
+    of the sliding window
+    Returns: a list of integers
+    """
+    maxima = [0] * (len(nums) - k + 1)
+    for i in range(len(nums) - k + 1):
+        maxima[i] = max(nums[i:i + k])
+
+    return maxima
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
-    k = 3
+    window_size = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print("Output of sliding_window_max function is: "
+          f"{sliding_window_max(arr, window_size)}")
