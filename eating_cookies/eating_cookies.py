c10fc2871c340484570cb2a3b9127b01509fb1ea
Elisabeth <elisabeth@github.com>
2020-06-09 09:38:14 -1000

    Eating cookies.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,14 +1,30 @@
-'''
-Input: an integer
-Returns: an integer
-'''
-def eating_cookies(n):
-    # Your code here
+"""
+Algorithms, problem 5
+"""
 
-    pass
+def eating_cookies(n, cache=None):
+    """
+    Input: an integer
+    Returns: an integer
+    """
+
+    # It's like the Fibonacci sequence, but summing the previous three elements
+    # instead of the previous two.
+    if n > 2:
+        ancestors = [1, 1, 2]
+        for _ in range(n - 2):
+            ancestors.append(sum(ancestors))
+            ancestors.pop(0)
+        return ancestors[2]
+
+    # Base cases, n <= 2.
+    if n == 2:
+        return 2
+    return 1
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster "
+          f"to eat {num_cookies} cookies.")
