7ad45d01b2c616230054f95b394dad784e3cb714
jennl97 <jennhott97@gmail.com>
2020-06-11 22:06:44 -0500

    First commit of algorithms project

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,6 +4,31 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+    
+    num_cookies = n
+    counter = 0
+
+    # base cases
+    if num_cookies <= 0:
+        # return 'There are no cookies in the cookie jar'
+        return counter
+        
+    if num_cookies == 1:
+        return counter + 1
+
+    if num_cookies == 2:
+        return counter + 2
+
+    if num_cookies == 3:
+        return counter + 4
+
+    # Jenn's psuedo code starts here
+    # Can only eat 3 cookies max at a time 
+
+    # if num_cookies > 3:
+        #if num_cookies % 3 = 1:
+            #
+
 
     pass
 
