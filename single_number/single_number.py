410fe69488305f1e71d03481697bbed1f04e39c6
Amy Barba <Amybarba70@gmail.com>
2020-06-15 21:46:55 -0700

    Single number complete

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,15 +1,19 @@
-'''
+"""
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
-'''
-def single_number(arr):
-    # Your code here
+"""
 
-    pass
+
+def single_number(arr, num):
+    # Your code here
+    res = arr[0]
+    for i in range(1, num):
+        res = res ^ arr[i]
+    return res
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
-    print(f"The odd-number-out is {single_number(arr)}")
+    print(f"The odd-number-out is {single_number(arr, len(arr))}")
