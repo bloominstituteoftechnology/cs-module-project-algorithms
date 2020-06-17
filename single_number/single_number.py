26cf011f882311e524ff12ef9063d964a6bf2cdc
CC <ciarabeitel@gmail.com>
2020-06-17 16:04:37 -0400

    Complete single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,42 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # takes in an array
+    # probably will loop over each number (for loop or while loop?)
+    # could we sort the array for faster looping?
+        # the array needs to have even number of elements to do comparison 
+    # do we need to declare any varibles, maybe a counter or index?
+    # may need to do some check of does this element == next element
+        # if so, that element is a duplicate
+        # else, this is the single number
+    
+    index = 0
+    single_element = None
+    length = len(arr)
 
-    pass
+    arr.sort()
+  
+    if length % 2 == 0: # even length of array (to compare a pair of numbers)
+        while index < length:
+            current_element = arr[index]
+            next_element = arr[index + 1]
+            if current_element == next_element:
+                index = index + 2
+            else:
+                single_element = current_element 
+                break # need to exit the loop if you found the number
+        return single_element
+    else: # odd length of array
+        last_element = arr[-1] # declare a variable to hold the last element in the array
+        while index < length-1: # loop through UNTIL the last element
+            current_element = arr[index]
+            next_element = arr[index + 1]
+            if current_element == next_element:
+                index = index + 2
+            else:
+                single_element = current_element 
+                break # need to exit the loop if you found the number
+        return single_element or last_element 
 
 
 if __name__ == '__main__':
