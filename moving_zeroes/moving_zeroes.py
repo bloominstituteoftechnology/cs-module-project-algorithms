652a943f11f2c95f48726ae4b5377b708ed090e3
Shawn Gee <shawngee@gmail.com>
2020-06-17 15:32:31 -0400

    Added solution for moving zeros that runs in O(n) time with O(n) space

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,33 +2,64 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # We can look at this problem as if we are moving non-zeros to the left
-    # But that would mean insertions at the beginning of the array
-    # Or we could look at it as moving zeros to the end of the array
-    # The challenge asks to do it in one pass with O(1) space
 
-    # Iterate through numbers
-    # If number is zero, move to end of array
-    # If number is not zero, simply increment the index
-    # We want to be looping through numbers minus number of zeros moved
-    # Let's use a while loop for our iteration so that we can modify the end
+# This solution takes O(1) space as requested by directions, but runs in O(n^2) time
+# def moving_zeroes(arr):
+#     # We can look at this problem as if we are moving non-zeros to the left
+#     # But that would mean insertions at the beginning of the array
+#     # Or we could look at it as moving zeros to the end of the array
+#     # The challenge asks to do it in one pass with O(1) space
+
+#     # Iterate through numbers
+#     # If number is zero, move to end of array
+#     # If number is not zero, simply increment the index
+#     # We want to be looping through numbers minus number of zeros moved
+#     # Let's use a while loop for our iteration so that we can modify the end
+
+#     i = 0
+#     end = len(arr)
+
+#     while i < end:
+#         if arr[i] == 0:
+#             arr.append(arr.pop(i))
+#             end -= 1
+#         else:
+#             i += 1
+
+#     return arr
+
+# This solution takes O(n) space but runs in O(n) time
+def moving_zeroes(arr):
+    result = [0] * len(arr)
 
     i = 0
-    end = len(arr)
+    j = len(arr) - 1
 
-    while i < end:
-        if arr[i] == 0:
-            arr.append(arr.pop(i))
-            end -= 1
-        else:
+    for num in arr:
+        if num != 0:
+            result[i] = num
             i += 1
-
-    return arr
-
+        else:
+            result[j] = num
+            j -= 1
+    
+    return result
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+
+
+import time
+
+zeros = [0] * 1000000
+
+start_time = time.time()
+
+moving_zeroes(zeros)
+
+end_time = time.time()
+
+print(f"Moving zeros took {end_time - start_time}")
