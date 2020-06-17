4474c6c1379dc6e3b82a1708cead12ae084942e1
CC <ciarabeitel@gmail.com>
2020-06-17 17:00:45 -0400

    Complete moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,10 +3,23 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # take an array of integers [0, 3, 1, 0, -2] arr
+    # loop 
+    # if number != 0, move to the left of the array
+    # if number == 0, capture and append
+    # variable for new array to hold moved numbers
+    # return new array
 
-    pass
-
+    index = 0
+    new_array = []
+    while index < len(arr):
+        number = arr[index]
+        if number == 0:
+            new_array.insert(len(arr), number)
+        else:
+            new_array.insert(0, number)
+        index += 1
+    return new_array
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
