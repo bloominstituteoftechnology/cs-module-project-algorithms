1cfba149e2870fcca400d2028a499fe725237bb7
Doina Langille <doina.jitoreanu@gmail.com>
2020-06-17 17:50:00 -0400

    sliding_window_max solution

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,9 +3,23 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    # initialize a resulted array
+    resulted_array = []
 
-    pass
+    # for each number in the list, except the last k-1
+    for i in range(len(nums)-(k-1)):
+        # i is a start index for window
+        # initialize an end index
+        end = i + k
+
+        # set the windows
+        window = nums[i:end]
+        # get the max number
+        max_num = max(window)
+        # add max number to the resulted array
+        resulted_array.append(max_num) 
+
+    return resulted_array
 
 
 if __name__ == '__main__':
