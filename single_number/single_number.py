ab738d0af7f5ba9b2f3aa375a9ae94affb4f4e67
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-06-17 17:08:19 +0300

    single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,28 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# from collections import Counter 
+
 def single_number(arr):
     # Your code here
-
-    pass
+    # Understand the problem:
+    # An array of numbers where every each of one appears twice except of one that it appears only once.
+    # Input = array =[3 2 5 2 5 3 1]
+    # Output = 1
+    
+    not_duplicate_list = []
+    nums = arr
+    # for every number in numbers:
+    for number in nums:# loop through numbers array
+        if number in not_duplicate_list:# remove any duplicate number
+             not_duplicate_list.remove(number)
+        else:
+            not_duplicate_list.append(number)# otherwise add the number to the list
+    return not_duplicate_list.pop()
+    
+    
+   
+    
 
 
 if __name__ == '__main__':
