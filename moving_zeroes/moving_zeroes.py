b00a97a17a1b57fbada22043f4be3470e9140e63
Shawn Gee <shawngee@gmail.com>
2020-06-17 15:11:00 -0400

    First pass at moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,28 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # We can look at this problem as if we are moving non-zeros to the left
+    # But that would mean insertions at the beginning of the array
+    # Or we could look at it as moving zeros to the end of the array
+    # The challenge asks to do it in one pass with O(1) space
 
-    pass
+    # Iterate through numbers
+    # If number is zero, move to end of array
+    # If number is not zero, simply increment the index
+    # We want to be looping through numbers minus number of zeros moved
+    # Let's use a while loop for our iteration so that we can modify the end
+
+    i = 0
+    end = len(arr)
+
+    while i < end:
+        if arr[i] == 0:
+            arr.append(arr.pop(i))
+            end -= 1
+        else:
+            i += 1
+
+    return arr
 
 
 if __name__ == '__main__':
