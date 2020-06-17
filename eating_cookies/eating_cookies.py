eea50ebf1031bafe4e618f52b734014207ad5408
Doina Langille <doina.jitoreanu@gmail.com>
2020-06-17 18:28:09 -0400

    eating_cookies solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,23 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    # base case
+    if n<0:
+        return 0
+    elif n == 0:
+        return 1
+    # if only 1 cookie
+    elif n==1:
+        return 1
+    # if two cookies:
+    elif n==2:
+        return 2
+    # if three or more cookies:
+    else:
+        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = -1
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
