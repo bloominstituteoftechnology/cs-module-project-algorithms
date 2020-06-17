8a4db93ede1c9b0c61c2acac1d0a88ee95db3b89
Doina Langille <doina.jitoreanu@gmail.com>
2020-06-17 17:14:33 -0400

    moving_zeroes solution

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,17 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # initialize altered_array
+    altered_array = []
+    zeros = []
 
-    pass
+    for element in arr:
+        if element != 0:
+            altered_array.append(element)
+        else:
+            zeros.append(element)
+    altered_array += zeros
+    return altered_array
 
 
 if __name__ == '__main__':
