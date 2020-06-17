6f05557e107479f99526f55c7c826702ea955b01
Doina Langille <doina.jitoreanu@gmail.com>
2020-06-17 17:08:30 -0400

    single_number solution

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,18 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # initialize a null list for single numbers
+    single_list = []
+    duplicate_list = []
 
-    pass
-
+    # go through all elements
+    for element in arr:
+        if element not in single_list:
+            single_list.append(element)
+        else:
+            duplicate_list.append(element)
+            single_list.remove(element)
+    return single_list[0]
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
