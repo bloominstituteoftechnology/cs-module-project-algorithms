b6952e2cd2c6918457ade4562cea532d32ef145e
Michael McGrath <maybemichael@icloud.com>
2020-06-17 15:47:20 -0700

    Finished Single Number and Moving Zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,12 +4,43 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    # start and ending index
+    start = 0
+    end = len(arr) - 1
+    # while start and end have not met 
+    while end > start:
+        # check end is 0 and start is not for swap
+        if arr[start] is 0 and arr[end] is not 0:
+            arr[start], arr[end] = arr[end], arr[start]
+            # next for both in their directions
+            start += 1
+            end -= 1
+        elif arr[start] is not 0 and arr[end] is not 0:
+            start += 1
+        # otherwise decrement end
+        else:
+            end -= 1
+    # return input list
+    return arr
 
-    pass
-
+arr = [1, 2, 3, 0, 4, 0, 0]
+answer = moving_zeroes(arr)
+print(answer)
+arr = [0, 0, 0, 0, 3, 2, 1] 
+answer = moving_zeroes(arr)
+print(answer)
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
 
-    print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+    print(f"The result of moving_zeroes is: {moving_zeroes(arr)}")
+
+import time
+zeroes = [0] * 1000000
+list1 = zeroes + [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 10]
+start_time = time.time()
+moving_zeroes(list1)
+end_time = time.time()
+print(list1[:100])
+print(f"Moving zeroes took {end_time - start_time}")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,10 +4,22 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    unique = set()
+    double_total = 0
+    single_total = 0
+    # loop through input list
+    for item in arr:
+        double_total += item
+        if unique.__contains__(item):
+            continue
+        else:
+            unique.add(item)
+            single_total += item
+    return (single_total * 2) - double_total
 
-    pass
-
-
+foo = [2, 2, -1, -1, -12]
+x = single_number(foo)
+print(x)
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
