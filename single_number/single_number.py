6104ec63ea82d38a8ac51e148b76d08205e4d79c
Doug Cohen <dougscohen@gmail.com>
2020-06-17 03:32:14 -0400

    initial commit: added sorting script and completed single_number.py. Test ran ok

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,37 @@
+from sorting import merge, merge_sort
+
 '''
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    
+    arr = merge_sort(arr)
+    
+    i = 0
+    j = 1
+    
+    # run this loop until I manually break it
+    while True:
+        
+        # if index j is outside the list range, return the last item in the list
+        if j >= len(arr):
+            return arr[i]
+            break
+        
+        # if value at index i equals value directly to its right, increment i
+        #. and j by 2 and continue the loop
+        elif arr[i] == arr[j]:
+            i += 2
+            j += 2
 
-    pass
+        # Otherwise, two values next to each other aren't equal, and we should
+        #. return the value to the right
+        else:
+            return arr[i]
+            break
+    
 
 
 if __name__ == '__main__':
