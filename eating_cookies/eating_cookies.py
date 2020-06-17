a9f81f44989985c30695e0bb7c403176310a5b0d
Lydia Zhang <art4everyan@gmail.com>
2020-06-17 16:55:04 -0400

    eating cookie

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,10 +5,21 @@ Returns: an integer
 def eating_cookies(n):
     # Your code here
 
-    pass
+    DP = [0 for i in range(0, n + 1)] 
+      
+    # base cases 
+    DP[0] = DP[1] = 1
+    DP[2] = 2
+  
+    # Iterate for all values from 3 to n 
+    for i in range(3, n + 1): 
+        DP[i] = DP[i - 1] + DP[i - 2] + DP[i - 3] 
+      
+    return DP[n] 
 
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
+print(eating_cookies(4))
+# if __name__ == "__main__":
+#     # Use the main function here to test out your implementation
+#     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+#     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
