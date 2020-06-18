df26b6d725dd1a0bccc01ddedc94fc8ff326aeb6
Michael McGrath <maybemichael@icloud.com>
2020-06-17 22:25:09 -0700

    Finished First Pass Solution for Product Of All Numbers

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,6 +2,25 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# First Pass Solution
+#  
+# def moving_zeroes(arr):
+#     # Your code here
+#     start = 0
+#     end = len(arr) - 1
+#     while end > start:
+#         if arr[start] is 0 and arr[end] is not 0:
+#             arr[start], arr[end] = arr[end], arr[start]
+#             start += 1
+#             end -= 1
+#         elif arr[start] is not 0 and arr[end] is not 0:
+#             start += 1
+#         else:
+#             end -= 1
+#     return arr
+
+# Second Pass Solution
+
 def moving_zeroes(arr):
     # Your code here
     # start and ending index
@@ -37,10 +56,10 @@ if __name__ == '__main__':
     print(f"The result of moving_zeroes is: {moving_zeroes(arr)}")
 
 import time
-zeroes = [0] * 1000000
+zeroes = [1] * 1000000
 list1 = zeroes + [0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 10]
 start_time = time.time()
 moving_zeroes(list1)
 end_time = time.time()
-print(list1[:100])
+print(list1[:12])
 print(f"Moving zeroes took {end_time - start_time}")
