6c35c78d7d39f3bb9447a8f1d3fa8f8455420239
Surfsol <surfsol@protonamil.com>
2020-06-18 19:48:54 -0300

    cookies, single number

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,11 +2,54 @@
 
 import sys
 
-def making_change(amount, denominations):
+# def change(amount, x)
+#   amount = amount - x
+
+#   if amount == 0:
+#     return 1
+#   elif amount < 0:
+#     return
+#   else:
+#     change(amount, x)
+
+# def making_change(amount, denominations):
+#   # Your code here
+#   print(len(denominations))
+#   #base case 
+#   for x in range(len(denominations)):
+#     amount = amount - denominations[x]
+#     if amount == 0:
+#       return 1
+#     elif amount < 0:
+#       return 
+#     else:
+#       making_change(amount, denominations)
+
+# print(making_change(10, [1, 5, 10]))
+
+# def making_change(amount, denominations):
   # Your code here
 
-  pass
+  #base case 
+  
 
+  # if amount == 0:
+  #   return 1
+  # elif amount < 0:
+  #   return 0
+  # elif len(denominations) <= 0 and amount > 0:
+  #   return 0
+  # else:
+  #   return making_change(amount - denominations[-1], denominations) + making_change(amount, denominations[0:-1])
+
+def making_change(amount, denominations):
+  ways = [0] * (amount + 1)
+  ways[0] = 1
+  for coin in denominations:
+    for higher_amount in range(coin, amount + 1):
+      remainder = higher_amount - coin
+      ways[higher_amount] += ways[remainder]
+  return ways[amount]
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
