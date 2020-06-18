320ef5cda28d68517d23f21540991f685ce19c58
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-18 20:15:22 +0100

    Eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,9 +3,12 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
