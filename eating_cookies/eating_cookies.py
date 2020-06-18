0566a6e7bede39de1e8ca24dbe2a1e0a7d9b716b
CC <ciarabeitel@gmail.com>
2020-06-18 13:27:41 -0400

    Better version of eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,20 +2,26 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    #base case: when there are no more cookies
-    if n == 0:
-        # we found a way to eat our cookies
-        return 1
-    # check for negative values
-    if n < 0:
-        # this is not a valid way to eat our cookies
-        return 0
+def eating_cookies(n, cache=None):
+    
+    if n < 0: # check for negative values
+        return 0 # getting to a negative number means we didn't find one of the ways
+
+    if n == 0: # base case: when there are no more cookies
+        return 1 # getting to 0, means we found a way to eat our cookies
+
+    if cache is None: # if no cache yet
+        cache = {} # create a cache
+
+    if n in cache: # check for previosuly computed answer in our cache
+        return cache[n] # return cache[n]
 
     # this represents our recurive case
     # 3 possible decisions
     # eat 1 cookie, 2 cookies, or 3 cookies
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+
+    return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
