984f0ec07427321ab2843180d68c92e17eff4d7b
Annette Odingo <odingoannette@gmail.com>
2020-06-18 04:32:16 +0100

    Plan for moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -13,3 +13,14 @@ if __name__ == '__main__':
     arr = [0, 3, 1, 0, -2]
 
     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+
+"""
+Plan
+    Loop through the array
+    Check for zeros
+    use list.remove and list.append
+    return index of 0 in array
+"""
+"""
+Pseudocode
+"""
