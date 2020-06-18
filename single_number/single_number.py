0fcd308a046cd8888ea8b29352f62e25027c30e9
Annette Odingo <odingoannette@gmail.com>
2020-06-18 04:10:39 +0100

    Plan for single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -13,3 +13,10 @@ if __name__ == '__main__':
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
+""""
+    PLAN
+  Create a variable ans  
+  Loop through arr list
+    - Use ^ to perform an XOR operation to remove the odd number
+  Return ans 
+"""
