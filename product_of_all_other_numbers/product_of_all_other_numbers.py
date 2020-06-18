d4c3c7db977f29ceed038cfe959df63151f5ccec
Annette Odingo <odingoannette@gmail.com>
2020-06-18 05:36:14 +0100

    Plan for product-of_all_numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -14,3 +14,14 @@ if __name__ == '__main__':
     arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+
+"""
+Plan
+    Create var mult
+    Loop through array
+        -Use * for multiplying array elements 
+        -Loop through array again
+            -Use / for dividing mult with item in list
+            -Use int to convert value to integers
+        -Return arr
+"""
