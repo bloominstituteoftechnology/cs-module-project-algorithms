9868e21e690b7520ed2964d57f625ce288222906
richknicks <rlynch@gmail.com>
2020-06-19 03:41:10 -0400

    update algorithms

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,14 +1,29 @@
-'''
+"""
 Input: a List of integers
 Returns: a List of integers
-'''
+"""
+
+
 def moving_zeroes(arr):
     # Your code here
+    start = 0
+    end = len(arr) - 1
+    while start < end:
 
-    pass
+        if arr[start] == 0:
+
+            if arr[end] != 0:
+                arr[start], arr[end] = arr[end], arr[start]
+                start += 1
+            end -= 1
+        else:
+            if arr[end] == 0:
+                end -= 1
+            start += 1
+    return arr
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
 
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,14 +1,20 @@
-'''
+"""
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
-'''
+"""
+
+
 def single_number(arr):
     # Your code here
+    for num in arr:
 
-    pass
+        if arr.count(num) == 1:
+            return num
+
+    return -1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
