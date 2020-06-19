9018993283f5bba65d0db927814e6fd2e142bfd6
Chris Hartig <chrishartig41@gmail.com>
2020-06-19 10:15:27 -0500

    assignment

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,6 +5,7 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
 
+    # Old Code:
     i = 0
     end_idx = len(arr) - 1
     while i < len(arr):
@@ -18,6 +19,22 @@ def moving_zeroes(arr):
         i += 1
     
     return arr
+
+    # Currently, it employs what I'm going to call a rollback-
+    # selection sort, which has a runtime of O(n+m-m), where 
+    # n is the length of the array and m is the number of zeroes
+    # in the array. I specify that the runtime is plus m minus m
+    # because every time a zero is found, it rolls back the index
+    # the search was looking at by one, but also decreases the index
+    # where it stops looking by one.
+
+    # Also, I do not think that I can improve this code in any way,
+    # considering it has an O(n) runtime, whereas a standard
+    # selection sort, as well as a bubble sort, would be O(nm)
+
+    # maybe if I used quicksort or mergesort, but then again
+    # the runtime with the old code for me is 0.001s, so I
+    # wouldn't be able to tell.
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
