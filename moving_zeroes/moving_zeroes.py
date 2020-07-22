6f12d3a711f6d65899d8163b58687aeeac379446
shengjiyang <samswank@gmail.com>
2020-07-22 11:00:56 -0600

    first pass moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,16 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    num = 0
+    while 0 in arr:
+        index = arr.index(0)
+        arr.pop(index)
+        num += 1
+    
+    for i in range(num):
+        arr.append(0)
+        
+    return arr
 
 
 if __name__ == '__main__':
