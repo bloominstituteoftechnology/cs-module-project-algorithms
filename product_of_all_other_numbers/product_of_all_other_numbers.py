b9ceca2c287e6f5ae3cb93402b45e0659abd78c6
Nick <ptnguyen1901@gmail.com>
2020-07-22 15:24:08 -0400

    product_of_all_other_numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -5,8 +5,18 @@ Returns: a List of integers
 
 
 def product_of_all_other_numbers(arr):
-    a = reduce(lambda x, y: x * y, arr, 1)
-    print(a)
+
+    result = []
+    for firstIndex in range(len(arr)):
+        product = 1
+        for secondIndex in range(len(arr)):
+            if firstIndex != secondIndex:
+                product *= arr[secondIndex]
+        result.append(product)
+
+    return result
+
+
 # if __name__ == '__main__':
 #     # Use the main function to test your implementation
 #     # arr = [1, 2, 3, 4, 5]
@@ -14,4 +24,5 @@ def product_of_all_other_numbers(arr):
 #
 #     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
 
-product_of_all_other_numbers([1, 7, 3, 4])
+a = product_of_all_other_numbers([1, 7, 3, 4])
+print(a)
