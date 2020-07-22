a7f8138ab14c3f168adce8493b0229b3bf801a42
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-22 18:08:03 -0400

    Small sliding window test passing

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -5,12 +5,58 @@ Returns: a List of integers
 def sliding_window_max(nums, k):
     # Your code here
 
-    pass
+    # going to have an array of k-length
+    point1 = 0
+    point2 = k #> can slice via nums[point1:point2]
+    # will need point1 and point2 to increment by 1 in order to iterate
+    # through nums
+    # also need to make sure point2 stays in range
+
+    # need placeholder for max_vals
+    max_vals = []
+
+    while point2 <= (len(nums)):
+        # slice k-len array of nums
+        # print("point2 before:", point2)
+        x = nums[point1:point2]
+        point1 += 1
+        point2 += 1
+        # print("x:", x)
+        # print("point2 after:", point2)
+
+        # now, need to find max value of x
+        val = max(x)
+        max_vals.append(val)
+
+    return max_vals
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
-    arr = [1, 3, -1, -3, 5, 3, 6, 7]
-    k = 3
+    # # Use the main function here to test out your implementation 
+    # arr = [1, 3, -1, -3, 5, 3, 6, 7]
+    # k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    # print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+
+    # ## Example
+    # ```
+    # Sample Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+    # Expected Output: [3,3,5,5,6,7] 
+    # Explanation: 
+
+    # Window position                Max
+    # ---------------               -----
+    # [1  3  -1] -3  5  3  6  7       3
+    # 1 [3  -1  -3] 5  3  6  7       3
+    # 1  3 [-1  -3  5] 3  6  7       5
+    # 1  3  -1 [-3  5  3] 6  7       5
+    # 1  3  -1  -3 [5  3  6] 7       6
+    # 1  3  -1  -3  5 [3  6  7]      7
+    # ```
+
+    my_list = [5,10,15,20]
+    # breakpoint()
+    # print(sliding_window_max(my_list, 3))
+
+    samp = [1,3,-1,-3,5,3,6,7]
+    print(sliding_window_max(samp, 3)) #> [3,3,5,5,6,7]
