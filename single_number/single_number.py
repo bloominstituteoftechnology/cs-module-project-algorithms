8889bfa3286bdd2d78c9d7be87a74ef9cf7e3917
wesley.moody <wgm3190@gmail.com>
2020-07-22 18:37:12 -0500

    single number.py passed

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,12 +2,38 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# ***First Pass***
 def single_number(arr):
     index = 0
     single_element = None
-    lenght = len(arr)
+    length = len(arr)
 
-    pass
+    arr.sort()
+    if length % 2 == 0:
+        while index < length:
+            current_element = arr[index]
+            next_element = arr[index + 1]
+            if current_element == next_element:
+                index = index + 2
+            else:
+                single_element = current_element
+                break
+
+        return single_element 
+
+    else:
+        last_element = arr[-1]
+        while index < length - 1:
+            current_element = arr[index]
+            next_element = arr[index + 1]
+            if current_element == next_element:
+                index = index + 2
+            else:
+                single_element = current_element
+                break
+        return single_element or last_element
+
+
 
 
 if __name__ == '__main__':
