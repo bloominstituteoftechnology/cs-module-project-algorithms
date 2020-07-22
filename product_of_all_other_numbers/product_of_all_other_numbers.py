e96dc884742bd22e5a45e5ffbee267d5532c5892
Michael-B1764 <mikebailar@gmail.com>
2020-07-22 13:52:29 -0700

    product od all is done

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,22 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+import math
 def product_of_all_other_numbers(arr):
-    # Your code here
 
-    pass
+    length = len(arr)
+    new_arr = []
+    for x in range(length):
+        new_arr.append(math.prod(arr[1:]))
+        num = arr.pop(0)
+        arr.append(num)
 
+    return new_arr
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # expect [120, 60, 40, 30, 24]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
