80a2c0eaaf2c59b6bace44d51a7899730d7e9d4e
Anthony Kawa <anthonyk2020@gmail.com>
2020-07-22 15:29:30 -0700

    passing in test_eating_cookies_small_n

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,16 @@
 Input: an integer
 Returns: an integer
 '''
+from itertools import combinations
 def eating_cookies(n):
-    # Your code here
-
-    pass
-
+    if n == 0:
+        return 1
+    elif n > 0 and n < 3:
+        return n
+    else:
+        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 50
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
