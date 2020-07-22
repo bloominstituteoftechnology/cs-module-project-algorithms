bef7ab0f4da8353b3584133f577fca4b05de952b
Krishna Dahal <krishna.dahal2017@gmail.com>
2020-07-22 19:02:59 -0400

    signle_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,33 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    index = 0
+    single_element = None
+    length = len(arr)
+    arr.sort()
 
-    pass
+    if length % 2 == 0: # even length of array (to compare a pair of numbers)
+        while index < length:
+            current_element = arr[index]
+            next_element = arr[index + 1]
+            if current_element == next_element:
+                index = index + 2
+            else:
+                single_element = current_element 
+                break # need to exit the loop if you found the number
+        return single_element
+    else: # odd length of array
+        last_element = arr[-1] # declare a variable to hold the last element in the array
+        while index < length-1: # loop through UNTIL the last element
+            current_element = arr[index]
+            next_element = arr[index + 1]
+            if current_element == next_element:
+                index = index + 2
+            else:
+                single_element = current_element 
+                break # need to exit the loop if you found the number
+        return single_element or last_element 
+
 
 
 if __name__ == '__main__':
