3715e88ab006a5db5bce7f8368cf05bfbf85f09d
wesley.moody <wgm3190@gmail.com>
2020-07-22 18:44:41 -0500

    product test passing

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,30 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# ***First Pass***
 def product_of_all_other_numbers(arr):
-    # Your code here
+    current_index = 0
+    new_array = []
+    length = len(arr)
 
-    pass
+    while current_index < length:
+        inside_index = 0
+        result = 1
+        while inside_index < length:
+            if inside_index == current_index:
+                inside_index += 1
+            else:
+                result = result * arr[inside_index]
+                inside_index += 1
+        new_array.append(result)
+        current_index += 1
+    return new_array
+
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
