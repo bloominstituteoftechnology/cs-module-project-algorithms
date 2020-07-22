d9c3252ff509647d75f39df78d1524815e8aa805
Anthony Kawa <anthonyk2020@gmail.com>
2020-07-22 12:23:27 -0700

    finished sliding window but takes 20s for large input test

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,10 +2,18 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+import time
 def sliding_window_max(nums, k):
-    # Your code here
-
-    pass
+    # assign empty list to maxs
+    maxs = []
+    # for loop through nums with the end being len(nums) - k
+    for i in range(0, len(nums) - (k - 1)):
+        # find max from index + k and append to maxs
+        current_max = max(nums[i:i+k])
+        maxs.append(current_max)
+    
+    # return maxs
+    return maxs
 
 
 if __name__ == '__main__':
