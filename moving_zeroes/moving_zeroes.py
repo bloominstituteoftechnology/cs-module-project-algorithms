aad7831d596c1805b7f52683a3479ac323386206
willsn <willsn@gmail.com>
2020-07-22 09:52:01 -0600

    moving zeroes and single number

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,11 +1,39 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+
+
+
+```
+Sample input: [0, 3, 1, 0, -2]
+Expected output: 3
+Expected output array state: [3, 1, -2, 0, 0]
+```
+
+```
+Sample input: [4, 2, 1, 5]
+Expected output: 4
+Expected output array state: [4, 2, 1, 5]
+```
+
+
+
 '''
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    ## go through each item in the list 
+    for item in arr:
+        ## if that item is not a zero , move it to the left size of the array
+        if item != 0:
+            arr.remove(item)
+            arr.insert(0, item)
+    
+    print(arr)
+    return arr
+    
+
+    #pass
 
 
 if __name__ == '__main__':
