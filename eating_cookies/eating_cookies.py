15c38227beb21fd61506f20ed9546a4790a3d131
unknown <coopervos1@gmail.com>
2020-07-22 16:12:17 -0500

    mvp

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,11 +1,32 @@
+import sys
+sys.setrecursionlimit(1500)
+
 '''
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     # Your code here
+    permutations = 0
+    if n == 0:
+        return 1
 
-    pass
+    if (n - 1) == 0:
+        permutations += 1
+        return permutations
+    permutations += eating_cookies(n - 1)
+
+    if n - 2 == 0:
+        permutations += 1
+        return permutations
+    permutations += eating_cookies(n - 2)
+
+    if n - 3 == 0:
+        permutations += 1
+        return permutations
+    permutations += eating_cookies(n - 3)
+
+    return permutations
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
