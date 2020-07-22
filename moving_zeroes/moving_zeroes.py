920973162f821dfc0e99e5522ab005eab1218ccb
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-07-22 10:25:01 -0700

    finished moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,21 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    index = 0
+    # Take an array of integers [0, 3, 1, 0, -2] arr
+    new_array = []
+    # loop 
+    while index < len(arr):
+        number = arr[index]
+        # If number == 0, capture and append
+        if number == 0:
+            new_array.insert(len(arr), number)
+        # If number != 0, move to the left of the array
+        else:
+            new_array.insert(0, number)
+        index += 1
+    # Variable for new array to hold moved numbers
+    return new_array
 
 
 if __name__ == '__main__':
