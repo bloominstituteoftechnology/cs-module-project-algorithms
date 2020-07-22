6eaccdb62adf1f3636952e48676394e7eb9acfde
wesley.moody <wgm3190@gmail.com>
2020-07-22 15:53:16 -0500

    COOOOOOOOOOKIES

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -13,13 +13,19 @@ def eating_cookies(n):
     
     else: 
         return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+    # here the computation is O(2**n) time complexity
 
+    implementing memoization and using cache allows work to be save
+    cache is a dictionary where keys is the n, value is the answer
+
+
+        
 
 
     
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 9
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
