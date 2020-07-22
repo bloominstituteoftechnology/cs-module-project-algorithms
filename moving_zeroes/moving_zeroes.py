1acb234ed7c15404b2911358e3f3b193c170ee54
Anthony Kawa <anthonyk2020@gmail.com>
2020-07-22 11:42:17 -0700

    completed moving zeroes and single number

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,25 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    # assign empty array to non_zero list
+    temp = [0] * len(arr) 
+    # initialize index of arr and temp
+    i = j = 0
+    # iterate through array
+    while i < len(arr):
+        # if i is not 0
+        if arr[i] != 0:
+            # assign value from arr to temp
+            temp[j] = arr[i]
+            # increment index for temp
+            j += 1
+        # increment index for arr
+        i += 1
+    # loop through temp and update arr with value from temp
+    for i in range(0, len(temp)):
+        arr[i] = temp[i]
+    # return mutated arr
+    return arr
 
 
 if __name__ == '__main__':
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,25 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-
-    pass
+    # assign empty array to single_ladies
+    single_ladies = []
+    # iterate through array
+    for i in range(0, len(arr)):
+    # if value at index i is in new list
+        if arr[i] in single_ladies:
+            # remove i from new list
+            single_ladies.remove(arr[i])
+        # else
+        else:
+            # append number to new list
+            single_ladies.append(arr[i])
+        # return first index of new list
+    return single_ladies[0]
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    
 
     print(f"The odd-number-out is {single_number(arr)}")
