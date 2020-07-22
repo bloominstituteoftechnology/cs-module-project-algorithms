00fe03ac19cfba898b41277be5c3e1899840ad06
Nick <ptnguyen1901@gmail.com>
2020-07-22 14:45:47 -0400

    moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,13 +3,21 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    holder = []
+    for num in arr:
+        if num == 0:
+            holder.append(num)
+        else:
+            holder.insert(0,num)
+    print(holder)
 
-    pass
+    return holder
 
 
-if __name__ == '__main__':
-    # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
+# if __name__ == '__main__':
+#     # Use the main function here to test out your implementation
+#     arr = [0, 3, 1, 0, -2]
+#
+#     print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
 
-    print(f"The resulting of moving_zeroes is: {moving_zeroes(arr)}")
+moving_zeroes([4,0,4,3,0])
