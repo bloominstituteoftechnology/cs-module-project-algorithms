2ed19352259ffeb4f80fd445d9e5c2495c818333
Mark Poggi <markp@mac.com>
2020-07-23 10:46:47 -0700

    Minor update to single_number exercise

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,9 +1,7 @@
-'''
+    '''
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-
-
 # def single_number(arr):
 #     # get length of arr
 #     n = len(arr)
