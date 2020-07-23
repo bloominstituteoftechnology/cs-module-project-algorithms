d548b60ffe6019fb954d9d0da270f2364abd1193
Teague <teaguehannam@gmail.com>
2020-07-22 22:52:16 -0600

    Fixed single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,16 +3,31 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-	unique = []
-	duplicated = []
-	for num in arr:
-		arr.remove(num)
-		if num not in arr and num not in duplicated:
-			return num
-		else:
-			duplicated.append(num)
 
-	return -1
+	# Set numbers in list to keys of a dictionary, initialize count to 0
+    counter = {value: 0 for value in arr}
+    #print(counter)
+    
+    # Count occurrence of each value
+    for value in arr:
+        counter[value] += 1
+    # print(counter)
+
+    # Return value that only occurred once. Runs until there's a count of 1
+    for value, count in counter.items():
+        if count == 1:
+            return value
+
+    raise ValueError(f'No values in {arr} occurred only once')
+
+
+	# duplicated = []
+	# for num in arr:
+	# 	arr.remove(num)
+	# 	if num not in arr and num not in duplicated:
+	# 		return num
+	# 	else:
+	# 		duplicated.append(num)
 
 
 if __name__ == '__main__':
