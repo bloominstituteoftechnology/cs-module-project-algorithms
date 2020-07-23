3ab38f9f0f72b91b2ff3baa1a0eb7f4735c5e021
Ezra Black <dev.ezrablack@gmail.com>
2020-07-23 00:41:36 -0500

    Day 1 Submission
    Not finished

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,9 +3,12 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    if n == 0 or n==1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
