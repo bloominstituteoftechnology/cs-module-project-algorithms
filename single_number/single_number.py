ec375c76a094cd4b5fe301b2be11f2a92ebb6b09
Drake Alia <DrakeTesla@MacBook-Pro.local>
2020-07-23 10:47:02 -0700

    improved runtime for single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,39 +2,60 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# def single_number(arr):
+#     # Your code here
+#     index = 0
+#     single_element = None
+#     length = len(arr)
+
+#     arr.sort()
+#     # Even length of array, to compare a pair of numbers
+#     if length % 2 == 0: 
+#         while index < length:
+#             current_element = arr[index]
+#             next_element = arr[index + 1]
+#             if current_element == next_element:
+#                 index = index + 2
+#             else:
+#                 single_element = current_element 
+#                 # Need to exit the loop if you found the number
+#                 break 
+#         return single_element
+#         # Odd length of array
+#     else: 
+#         # Declare a variable to hold the last element in the array
+#         last_element = arr[-1]
+#         # loop through until the last element 
+#         while index < length-1:
+#             current_element = arr[index]
+#             next_element = arr[index + 1]
+#             if current_element == next_element:
+#                 index = index + 2
+#             else:
+#                 single_element = current_element 
+#                 break
+#         return single_element or last_element 
+
+# Optimized
 def single_number(arr):
-    # Your code here
     index = 0
     single_element = None
     length = len(arr)
-
     arr.sort()
-    # Even length of array (to compare a pair of numbers)
-    if length % 2 == 0: 
-        while index < length:
-            current_element = arr[index]
-            next_element = arr[index + 1]
-            if current_element == next_element:
-                index = index + 2
-            else:
-                single_element = current_element 
-                # Need to exit the loop if you found the number
-                break 
-        return single_element
-        # Odd length of array
-    else: 
-        # Declare a variable to hold the last element in the array
+
+    if length % 2 == 1:
+        length = len(arr)-1
+    while index < length: 
+        current_element = arr[index]
+        next_element = arr[index + 1]
         last_element = arr[-1]
-        # loop through until the last element 
-        while index < length-1:
-            current_element = arr[index]
-            next_element = arr[index + 1]
-            if current_element == next_element:
-                index = index + 2
-            else:
-                single_element = current_element 
-                break
-        return single_element or last_element 
+        if current_element == next_element:
+            index = index + 2
+        else:
+            single_element = current_element 
+# To exit the loop if you found the number
+            break
+    return single_element or last_element
 
 
 if __name__ == '__main__':
