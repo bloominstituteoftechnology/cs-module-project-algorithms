ddfeb888191127f1f9171d029148f1e72882cc0c
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-07-22 20:37:37 -0700

    Worked on cookies and algorithms

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,8 +4,16 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+if n == 0:
+        return 1
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
 
-    pass
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
