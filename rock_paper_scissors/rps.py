0334c09cfdc17c1ce40555307bed40b8a2e253ff
Krishna Dahal <krishna.dahal2017@gmail.com>
2020-07-23 21:28:43 -0400

    moving zero optimited, product of all other numbers done, sliding window done

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,8 +4,22 @@ import sys
 
 def rock_paper_scissors(n):
   # Your code here
+  if n == 0:
+    return [[]]
+  moves = [["rock"], ["paper"], ["scissors"]]
+  return helper(n, moves)
 
-  pass
+def helper(n, moves):
+  if len(moves[0]) == n:
+    return moves
+  result = []
+  for move in moves:
+    for add_on in ["rock", "paper", "scissors"]:
+      curr = move[:]
+      curr.append(add_on)
+      result.append(curr)
+  return helper(n, result)
+
 
 
 if __name__ == "__main__":
