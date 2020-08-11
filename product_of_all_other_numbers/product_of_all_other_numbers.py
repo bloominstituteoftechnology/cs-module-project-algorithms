45179eaaac03391ffe2d4a9390e5e35442a7b559
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-08-10 21:45:23 -0600

    work on first pass

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,33 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# This is the first pass of the solution
+# Will loop through the list using the index
+# will have a second loop within the  first loop 
+# that will then multiply everthing found in the array
+# except at the index of the first array
 def product_of_all_other_numbers(arr):
-    # Your code here
+    new_array = []
+    
+    for i in range(len(arr)):
+        value = 1
+        # second loop
+        for k in range(len(arr)):
+            if i != k:
+                # do the multiplication here
+                value *= arr[k]
+        # appending to the array
+        new_array.append(value)
+    return new_array
 
-    pass
+    
+
+   
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
+    #arr = [1, 2, 3, 4, 5]
     arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,11 +2,47 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+# this is the first pass solution
+# this will loop through the array and will keep the number that doesn't have any 
+# number equal to it
+# def single_number(arr):
+#     # will loop through the array and will put the 
+#     # numbers in array 
+#     # will check if the number is in a second array.
+#     # If the number is not in the array then will add the number to the array.
+#     # If the number is in the array already then the number will be removed from the array.
+#     single = []
+#     for i in range(len(arr)):
+#         if arr[i] not in single:
+#             single.append(arr[i])
+#         else:
+#             single.remove(arr[i])
+#     return single[0]
+
+
+
+
+# This is the second pass solution that has the space complexity  of o(1)
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    # has to flags
+    
+    val = None
+    # looping through the arr
+    # will be popping off the value at the index asked for 
+    # then will ask if the value is in the rest of the
+    # array. If it is not then will return the value 
+    # If it is in the array will remove pop this value again from the
+    # array to make the array smaller
+    # Doing a while loop
+    # will continue to loop until there is just one element
+    while len(arr) > 1:
+        val = arr.pop() # removing the last element in the list
+        if val in arr:
+            arr.remove(val)
+        else:
+            break
+    return val
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
