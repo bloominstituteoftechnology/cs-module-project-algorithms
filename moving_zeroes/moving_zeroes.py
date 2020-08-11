50259e8196980d6c11f117dcb213479cfef1381e
David Wright <davidtwright89@gmail.com>
2020-08-10 23:39:45 -0400

    Implemented a first pass solution for moving_zeroes.py

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,16 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    zeros = []
+    i = 0
+    while i < len(arr):
+        if arr[i] == 0:
+            zeros.append(arr.pop(i))
+        else:
+            i += 1
+    
+    arr.extend(zeros)
+    return arr
 
 
 if __name__ == '__main__':
