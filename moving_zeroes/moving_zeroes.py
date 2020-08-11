0e9f80b6c06e0a3ebee9a7e786190a9bed7bdbf6
jpeone <jesse.gene.peone@gmail.com>
2020-08-10 19:32:11 -0700

    Finished simple write up and first pass at moving zeros

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -5,7 +5,31 @@ Returns: a List of integers
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    # okay so we have current index, and we have next index, and compare. 
+    # If current is zero swap.
+    # if current is zero and next is zero, increment next index and verify less 
+    # than the array size.
+
+    length = len(arr)
+
+    if length < 2:
+        return arr
+
+    i = 0
+    j = 1 #starting it one past the start
+
+    while j < length:
+        if arr[i] == 0:
+            if arr[j] == 0:
+                j += 1
+            else:
+                arr[i] = arr[j]
+                arr[j] = 0
+        else:
+            i += 1
+            j += 1
+
+    return arr
 
 
 if __name__ == '__main__':
