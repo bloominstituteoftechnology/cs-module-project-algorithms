76dcd3afc6510f8765ab915d89ce6a51d2bd9fed
David Wright <davidtwright89@gmail.com>
2020-08-11 00:15:54 -0400

    Implemented a first pass solution for eating_cookies.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,9 +3,12 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+    
+    return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
