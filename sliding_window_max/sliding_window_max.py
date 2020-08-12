6f426832b87f7385da2806c19b0ed52f79bafb27
jpeone <jesse.gene.peone@gmail.com>
2020-08-11 18:51:18 -0700

    sliding window first pass, brutish, faild second test

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,9 +3,30 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
+    # First Pass:
+    # Assuming K is never bigger than the array
+    # start with index, then look i + 1, i + 2... i + k
+    # store the greatest in the output array
+    # check that i + k is never greater than the last index
+    # return the output array
+    # this is O(kn) time.  Since k isn't a constant, this can get ugly
+    # but this is just a first pass
+    output = []
 
-    pass
+    for i in range(len(nums)):
+        # I did this with explicit minuses so that I didn't lose track
+        # of the zero indexing. If we think of a window as primarly being
+        # indices, then we have to offset k by one for zero indexing.
+        if i + (k - 1) > (len(nums) - 1):
+            return output
+
+        greatest = nums[i]
+
+        for j in range(k):
+            if greatest < nums[i+j]:
+                greatest = nums[i+j]
+            
+        output.append(greatest)
 
 
 if __name__ == '__main__':
