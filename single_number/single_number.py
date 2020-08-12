d9e328d44a630c7221ffda9ade7b6798dc53719f
jpeone <jesse.gene.peone@gmail.com>
2020-08-11 22:51:24 -0700

    single number optimal finished, passes all tests

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,22 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    # Firstpass: would be to make a hash table, iterate through the array
+    # and count the values in the hash table, then traverse the hash table and
+    # output the value with only 1.  This is O(n) and not single pass.
+    # single pass requires the exclusive or (xor) operation. So it relies on 
+    # the fact that duplicates fail and exclusiveor, so if you chain every 
+    # number with an exclusiveor, only the single one will true out.
 
-    pass
+    #so I take value1 xor value2 xor value3 etc.  Best way to do this is with
+    # a loop.
+
+    xor_chain = arr[0]
+
+    for i in range(1, len(arr)): #I already assigned the first element
+        xor_chain = xor_chain ^ arr[i]
+
+    return xor_chain
 
 
 if __name__ == '__main__':
