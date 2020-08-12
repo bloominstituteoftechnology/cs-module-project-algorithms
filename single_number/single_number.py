a270f51eccbfab5c3183f3eb28dcdfde418044a6
Joel <joelgroomer@gmail.com>
2020-08-11 19:08:37 -0700

    single_number complete

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,25 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
+    # Understand:
+    # A list of ints where every element is duplicated except one
+    # Return that one non-duplicated value
+    # Assume there's always only one number that isn't duplicated
 
-    pass
+    # Plan:
+    # Cycle through the list
+    #   call the count() list method to determine if there are 1 or 2 copies
+    #   if one, return that value. otherwise, keep going.
+
+    # Execute:
+
+    for val in arr:
+        if arr.count(val) == 1:
+            return val
 
 
 if __name__ == '__main__':
