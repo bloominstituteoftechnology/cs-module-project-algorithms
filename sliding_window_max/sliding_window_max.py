5ff46b65495ddf77325002652d05a23c52da7cf8
Joel <joelgroomer@gmail.com>
2020-08-11 20:29:46 -0700

    sliding_window_max done but not in O(n) time

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,27 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+
 def sliding_window_max(nums, k):
     # Your code here
+    def get_max(arr):
+        max = None
+        for val in arr:
+            if max is None or max < val:
+                max = val
+        return max
 
-    pass
+    results = []
+    for i in range(len(nums) - k + 1):
+        results.append(get_max(nums[i:i+k]))
+    return results
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
