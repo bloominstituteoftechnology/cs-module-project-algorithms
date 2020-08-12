e7d5fbdc58e3244128283b984463015fd760f7ca
RyanSWest <west56309@gmail.com>
2020-08-12 00:10:33 -0500

    moving zeroes passes test now

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,30 +2,40 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-
-x =150
-s = str(x)
-y = s.split(',')
-print(y)
 def moving_zeroes(arr):
-    # Your code here
-    new_arr = []
-    zarr=[]
-    for i in arr:
-        s = str(i)
-        s.split(',')
+    zero_count = arr.count(0)
+    curr_count =0
+    
+    while curr_count<zero_count:
+        arr.pop(arr.index(0))
+        arr.append(0)
+        
+        curr_count+=1
+    return arr
+ 
+# def moving_zeroes(arr):
+#     # Your code here
+#     new_arr = []
+#     zarr=[]
+#     for i in arr:
+#         s = str(i)
+#         s.split(',')
          
-        new_arr.append(s)
-        for i in new_arr:
-            if '0' in i:
-               zarr.append(i)
-               new_arr.remove(i)
-               
-    return new_arr + zarr 
+#         new_arr.append(s)
+#         for i in new_arr:
+#             if '0' in i:
+#                zarr.append(i)
+#                new_arr.remove(i)
+#                arr = new_arr+zarr
+#         for i in arr:
+#             int(i)       
+#     return arr
 
 
-stuff = [10,3,4,5,20]
-print("THIS",moving_zeroes(stuff)) 
+# stuff = [10,3,4,5,20]
+# print("THIS",moving_zeroes(stuff)) 
+
+
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
