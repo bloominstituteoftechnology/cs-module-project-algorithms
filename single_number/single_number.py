83013c9e7c88b30b8b0a2dad3b0648fd4ba5f708
John Kouris <jdkouris@gmail.com>
2020-08-11 18:51:23 -0700

    Finished 1st pass for single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,16 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    num_dict = {}
+    for num in arr:
+        if num in num_dict:
+            num_dict[num] += 1
+        else:
+            num_dict[num] = 1
+    for key, value in num_dict.items():
+        if value == 1:
+            return key
 
-    pass
 
 
 if __name__ == '__main__':
