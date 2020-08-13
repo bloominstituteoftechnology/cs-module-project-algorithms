2e63816893c07857afba10cd224b1fe094fb0e95
David Wright <davidtwright89@gmail.com>
2020-08-13 05:02:46 -0400

    Implemented a better solution for single_number.py

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,15 +3,11 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    set_1, set_2 = set(), set()
+    result = 0
 
     for n in arr:
-        if n not in set_1:
-            set_1.add(n)
-        else:
-            set_2.add(n)
-
-    return (set_1 - set_2).pop()
+        result ^= n
+    return result
 
 
 
