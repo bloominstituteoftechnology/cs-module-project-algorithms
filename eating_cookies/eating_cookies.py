bd07cafa2f2b9073306c134cd1f975fd509e6343
John Kouris <jdkouris@gmail.com>
2020-08-13 19:04:58 -0700

    Updated eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,18 +4,20 @@ Returns: an integer
 '''
 def eating_cookies(n, cache=None):
     # Your code here
-    if n < 0:
-        return 0
-
-    if n == 0:
+    if cache is None:
+        cache = {}
+    if n == 0 or n == 1:
         return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
     
-    elif cache is not None and cache[n] > 0:
+    if cache and cache[n]:
         return cache[n]
-    else:
-        if cache is None:
-            cache = [0 for i in range(n + 1)]
-        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    
     return cache[n]
 
 if __name__ == "__main__":
