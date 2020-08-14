b8e0258ac77fcbc3b0fdc1ae8afd23df4420506c
Jessie <jessie.griffin@icloud.com>
2020-08-13 23:33:43 -0400

    Eating Cookies not quite there yet

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,19 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+    # Use recursion?
+    if n == 0 or n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+    if n > 2:
+        return eating_cookies(n * (n - 3)) + 3
 
-    pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
