5010aa74749cef327d6a35ac40db8e8e4d666e1d
Jessie <jessie.griffin@icloud.com>
2020-08-13 21:48:05 -0400

    First Pass for sliding window max complete

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,10 +3,20 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
-    # Your code here
-
-    pass
-
+    # check if the length of the array is greater than or equal to the size of the window, if not return
+    max_nums = []
+    if len(nums) >= k:
+        # each time you loop through the array, create a new array of the elements inside the window
+        for i in range(len(nums) - k + 1):
+            window_nums = []
+            for j in range(k):
+                window_nums.append(nums[i + j])
+            max_num = max(window_nums)
+            # use the max function to find the max of the newly created array
+            max_nums.append(max_num)
+        return max_nums
+    else:
+        return
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
