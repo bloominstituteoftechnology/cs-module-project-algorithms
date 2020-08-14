3120214db15fae39edb0a35ea694105c2f08abe4
Jessie <jessie.griffin@icloud.com>
2020-08-13 22:16:48 -0400

    First Pass for move zeroes complete

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -8,52 +8,40 @@ def moving_zeroes(arr):
     # or move zeros to the right and ignore non zero numbers
     # if not zero, swap with last zero
     zero_idx_array = []
-
-    for i in range(len(arr) - 1):
-        if arr[i] == 0:
+    """
+    for i, e in enumerate(arr):
+        if i == e == 0:
             arr[i], arr[i+1] = arr[i+1], arr[i]
+            print(arr)
     return arr
     """
-    for i in range(len(arr)):
-        if arr[i] == 0:
-            z = i
-        if arr[i] != 0:
-            arr.insert(z, arr[i])
-            arr.pop(arr[i+1])
-    
-    # new implementation
+    count_zero = arr.count(0)
+    while 0 in arr:
+        arr.remove(0)
+
+    for n in range(count_zero):
+        arr.append(0)
+    return arr
+    """
+    count = 0
+    for i, z in enumerate(arr):
+        if z == 0:
+            arr.pop(i)
+            count += 1
+    for z in range(count):
+        arr.append(0)
+    return arr
+
     for i in range(len(arr)):
         if arr[i] == 0:
             zero_idx_array.append(i)
-    print(zero_idx_array)
     for z in zero_idx_array:
-        arr.append(0)
-        del arr[]
-    # new implementation
-    i = 0
-    while i < len(arr) - 1:
-        if arr[i] == 0:
-            arr.append(0)
-            del arr[i]
-            i = 0
-        else:
-            i += 1
-               
-    for i, z in enumerate(arr):
-        print("i = ", i)
-        if z == 0:
-            arr.append(0)
-            print("arr[i] = ", arr[i])
-            del arr[i]
-        
-        print(arr)
+        for n in range(len(arr))
     """
-    return arr
-
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
-    arr = [0, 3, 1, 0, -2]
-    #arr = [0, 0, 0, 0, 3, 2, 1]
+    #arr = [0, 3, 1, 0, -2]
+    arr = [0, 0, 0, 0, 3, 2, 1]
     #arr = [0, 0, 1, 0, -2]
 
     print(f"The result of moving_zeroes is: {moving_zeroes(arr)}")
