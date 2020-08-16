aef77be339bb29c80cbeff5d81998c03d2ba1b5d
Jose <pjose14.jp38@yahoo.com>
2020-08-15 21:50:07 -0500

    single_number completed

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,12 +4,21 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
-
-    pass
+    while len(arr) > 0:
+        #select number and remove it from list
+        number = arr.pop(0)
+        
+        #if number is not in the list, return single number 
+        if number not in arr:
+            return number
+        
+        #else remove it by the value
+        else: 
+            arr.remove(number)
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
-    print(f"The odd-number-out is {single_number(arr)}")
+    print(f"The odd number is {single_number(arr)}")
