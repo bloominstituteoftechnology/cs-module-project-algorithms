89817ad4f1707f509f770b9ded0cc7a94409cea9
Lambda_School_Loaner_277 <lambda_school_loaner_277@Lambda-School-Loaner-277s-MacBook-Pro.local>
2020-08-19 00:43:16 -0400

    2 tess passing

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,14 +1,22 @@
 '''
-Input: a List of integers where every int except one shows up twice
-Returns: an integer
-'''
-def single_number(arr):
-    # Your code here
+  Input: a List of integers where every int except one shows up twice
+  Returns: an integer
+  '''
 
-    pass
+
+def single_number(arr):
+
+    # set(arr) returns the array without duplicate instances of each number, and ordered numerically
+    # so [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    # returns {0, 1, 3, 4, 5, 9}
+    # so sum(set(arr)) returns 22, the sum of that returned set.
+
+    # return 2 * 22 - 35 = 9
+    return 2 * sum(set(arr)) - sum(arr)
 
 
 if __name__ == '__main__':
+
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
