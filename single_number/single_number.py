85919e11d4e9929f9e3087a46b8564f94850cf29
Anna Morris <annapatton444@gmail.com>
2020-09-08 20:33:58 -0500

    Update single_number.py

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
-    print(f"The odd-number-out is {single_number(arr)}")
+    print(f"The odd-number-out is {single_number(arr)}, expected 9")
