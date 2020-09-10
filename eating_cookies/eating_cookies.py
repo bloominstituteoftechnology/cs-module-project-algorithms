95b0dc3f60fb206c50ab0bd4b8fbf48f5530f21c
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-09-09 21:02:46 -0400

    eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,9 +3,16 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
+    # BASE CASE: If cookies eaten is more than n return 0
+    eaten = 0
+    if eaten > n:
+        return 0
+    # If eaten is the number of available cookies, return a count of 1
+    if eaten == n:
+        return 1
 
-    pass
+    # Else, use recursion to loop through each potential options incrementing eaten by potential options
+    return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
