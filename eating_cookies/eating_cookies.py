a2f40dbe36888c96a50fb1ca31fa1da5023829fd
Ariunbold <ariun1118@gmail.com>
2020-09-10 18:41:19 -0400

    caching eating_cookies for large data

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,16 +4,19 @@ Returns: an integer
 '''
 
 
-def eating_cookies(n):
+def eating_cookies(n, cache = None):
     # Your code here
-    if n < 2:
+    if n < 0:
+        return 0
+    if n == 0:
         return 1
-    elif n == 2:
-        return 2
-    # elif n == 3:
-    #     return 4
+    elif cache and cache[n] > 0:
+        return cache[n]
     else:
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+        if cache is None:
+            cache = [ 0 for i in range(n+1)]
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        return cache[n]
 
 
 if __name__ == "__main__":
