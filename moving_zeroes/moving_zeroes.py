e5feda3c2839f445c92dad1cdecb9780f61fa9bc
thegroovetrain <ericthegroovetrain@gmail.com>
2020-09-10 19:24:42 -0400

    whee

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,11 +1,19 @@
-'''
+"""
 Input: a List of integers
 Returns: a List of integers
-'''
-def moving_zeroes(arr):
-    # Your code here
+"""
 
-    pass
+
+def moving_zeroes(arr):
+    zeros = 0
+    out = []
+    for n in arr:
+        if n == 0:
+            zeros += 1
+        else:
+            out.append(n)
+    out.extend([0] * zeros)
+    return out
 
 
 if __name__ == '__main__':
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,23 @@
-'''
+"""
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
-'''
-def single_number(arr):
-    # Your code here
+"""
 
-    pass
+
+# O(n) time, O(n) memory
+def single_number(arr):
+    # hash table
+    ht = {}
+    # for each item in the array
+    for a in arr:
+        # if its in the hash table, remove it from the hash table
+        if a in ht:
+            ht.pop(a)
+        # otherwise, add it to the hash table
+        else:
+            ht[a] = 1
+    # there should only be one item in the hash table at this point.
+    return list(ht.keys())[0]
 
 
 if __name__ == '__main__':
