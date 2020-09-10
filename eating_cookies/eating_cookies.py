6b7ac2cea5efe533ef02f2c83e751a4cab65fcdc
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-09-10 14:00:08 -0400

    MVP

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,17 +2,41 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # BASE CASE: If cookies eaten is more than n return 0
-    eaten = 0
-    if eaten > n:
-        return 0
-    # If eaten is the number of available cookies, return a count of 1
-    if eaten == n:
-        return 1
+def eating_cookies(n, cache={}):
+    ''' FIRST PASS SOLUTION '''
 
-    # Else, use recursion to loop through each potential options incrementing eaten by potential options
-    return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+    # # BASE CASE: If n is less than 0, return a count of 0
+    # if 0 > n:
+    #     return 0
+
+    # # If n is 0, return a count of 1
+    # if n == 0:
+    #     return 1
+
+    # # Else, use recursion to loop through each potential options incrementing eaten by potential options
+    # return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+
+    ''' WRITING BETTER SOLUTIONS '''
+
+    # Use cache for memoization of recursive calls to the method
+    # If cache is empty, set it to be a range
+    if not cache:
+        cache = [0 for i in range(n + 1)]
+
+    # BASE CASE: If n is less than 0, return a count of 0
+    if 0 > n:
+        return 0
+
+    # If n is 0, return a count of 1
+    if n == 0:
+        cache[n] = 1
+
+    # If n not in cache, use recursion to loop through each potential options and storing to the cache
+    if cache[n] == 0:
+        cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+
+    return cache[n]
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,24 +2,54 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+from collections import deque
+
 def sliding_window_max(nums, k):
-    # Look for the maximum value within k throughout the nums arr and store those values in a result list
-    # If len(k) - 1 > len(nums) - 1 return because the sliding window is no longer fitting within the list
-    # Start at index 0 and create a subarray of len k (slice nums to be nums[index:k])
-    # After each pass increment index by 1
-    # Use recursion to go through the full nums list
+    ''' FIRST PASS SOLUTION '''
+
+    # # Look for the maximum value within k throughout the nums arr and store those values in a result list
+    # result = []
+    # index = 0
+    # end_index = k
+
+    # # If len(k) - 1 > len(nums) - 1 return because the sliding window is no longer fitting within the list
+    # while index <= len(nums) - k:
+    #     # Start at index 0 and create a subarray of len k (slice nums to be nums[index:k])
+    #     temp_nums = nums[index:end_index]
+    #     max_value = max(temp_nums)
+    #     result.append(max_value)
+
+    #     # After each pass increment indexes by 1
+    #     index += 1
+    #     end_index += 1
+
+    # return result
+
+    ''' WRITING BETTER SOLUTIONS '''
+
+    # Setup result list to hold max values and a deque
     result = []
-    index = 0
-    end_index = k
+    dq = deque()
 
-    while index <= len(nums) - k:
-        temp_nums = nums[index:end_index]
-        max_value = max(temp_nums)
-        result.append(max_value)
-        index += 1
-        end_index += 1
+    # Loop through nums list
+    for i, num in enumerate(nums):
+        # While the element in nums at the last dq index is smaller than the current num, remove it from the deque
+        while dq and nums[dq[-1]] < num:
+            dq.pop()
 
-    return result
+        # If the current index minus the first deque index is greater than k, remove the first element since it's out of range for the slide    
+        if dq and i - dq[0] >= k:
+            dq.popleft()
+
+        # Add index to the deque list    
+        dq.append(i)
+
+        # Append the first deque index to the results list
+        result.append(nums[dq[0]])
+
+    # Return the result list starting at k - 1
+    return result[k - 1:]
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
