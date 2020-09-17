5f96f5f01308b50f8b877a7e32c32e6cfe3bf13a
Natalia <nataliamirandac@hotmail.com>
2020-09-17 15:07:40 -0400

    single_number - passed

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,8 +4,25 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    # Check every element if it appears once or not.
+    #once the element with single occurence is found,
+    #return it 
 
-    pass
+    #Better solution:
+    #XOR of a number with itself is 0
+    #XOR of a number with 0 is number itself
+    #XOR operator is ^
+    # res = 7 ^ 3 ^ 5 ^ 4 ^ 5 ^ 3 ^ 4
+    # XOR ->  res = 7 ^ (3 ^ 3) ^ (4 ^ 4) ^ (5 ^ 5)
+    #         res = 7 ^ 0 ^ 0 ^ 0
+    #         res = 7 ^ 0 = 7
+    # XOR a number with itself is 0
+    # Since i ^ i = 0 for any integer i, and i ^ 0 = i
+    result = 0
+    for i in arr:
+        result ^= i
+    return result
+
 
 
 if __name__ == '__main__':
