6b0d9918ee5df87fa10fef316845c8d5cecb247d
Kelson Hartle <kelson99hartle@icloud.com>
2020-09-25 11:55:53 -0600

    cookies test now passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,28 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+def eating_cookies(desired):
+    # recursive 
+    # cookie monster can eat 1, 2, 3 cookies at a time
+    current = desired
+    if current > desired:
+        return 0
+    if current == desired:
+        return 1
 
-    pass
+    else: 
+        current += 1
+
+    return eating_cookies(desired) + eating_cookies(desired) + eating_cookies(desired)
+    
+ok = eating_cookies(5)
+print(ok)
+    
+
+    
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    # print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
