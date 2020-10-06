c8db16a2542cdf634efda65d3191d11ff6b0488f
jarparrish <jarparrish@gmail.com>
2020-10-06 15:17:26 -0700

    single number complete

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,22 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # Make an empty list to keep of all values, where the length of the list is == to the value of the largest number
 
-    pass
+    instance_list = [0] * (max(arr) + 1)
+    print(instance_list)
+
+    #loop through arr, increase the value at the instance_list index that corresponds to the value in arr
+
+    for value in arr:
+        instance_list[value] += 1
+    print(instance_list)
+
+    #loop through the instance_list and return the index where instance_list[i] == 1
+
+    for i in range(len(instance_list)):
+        if instance_list[i] == 1:
+            return i
 
 
 if __name__ == '__main__':
