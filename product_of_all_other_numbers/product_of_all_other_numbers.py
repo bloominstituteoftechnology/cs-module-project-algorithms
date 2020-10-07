d35726a3e24fd2de5bb7833faefd37c15a4281cd
unknown <27julioescalera27@gmail.com>
2020-10-06 20:09:47 -0700

    Product of all other nubmers done

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,11 +1,27 @@
-'''
-Input: a List of integers
-Returns: a List of integers
-'''
+# https://discuss.codecademy.com/t/challenge-product-of-everything-else/176317
 def product_of_all_other_numbers(arr):
-    # Your code here
-
-    pass
+    """
+    Write a function that receives an array of integers and returns an array
+    consisting of the product of all numbers in the array except the number at that index.
+    :param arr: a list of integers
+    :return: a list of integers
+    """
+    # so someway to exclude the index while we are doing multiplying
+    # results to keep the result of all the products except index
+    result = 1
+    # product_list to put each product for each value
+    product_list = []
+    # loop through the arr
+    for i in range(len(arr)):
+        # access each number in the arr
+        for n in arr:
+            if n != arr[i]:
+                # if n doesn't equal the current number for i then multiple it
+                result *= n
+        product_list.append(result)
+        # restart
+        result = 1
+    return product_list
 
 
 if __name__ == '__main__':
