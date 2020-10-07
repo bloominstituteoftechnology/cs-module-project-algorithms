eb2b17d1196d9fa3e3ae8c14fe97fc46dcdd5c72
Cai Nowicki <cai.nowicki@gmail.com>
2020-10-06 22:18:34 -0400

    first pass on single_number, starting on moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,6 +1,8 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+Need all non-zero integers on left side of array, all zeroes on right side
+One traversal, O(1) space
 '''
 def moving_zeroes(arr):
     # Your code here
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,25 @@
 '''
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
+Need O(1) space complexity, which means doing the traversal in place, rather than creating new arrays, and without
+changing the shape of the given array. O(1) space complexity allows for creation of pointers (even multiple pointers)
+as long as the number of pointers is static, not related to n of list
+Attempt to find doubled int on one traversal through array
 '''
 def single_number(arr):
-    # Your code here
+    # need matching pointer to check against l[0].
+    # if no match is found, step pointer forward until a match is found. discard values. now l[0] is a new value.
+    # if second reaches end of list with no pair found, l[0] is result
+    pointer = 1
+    while len(arr) > 1 and pointer < len(arr):
+        if arr[0] == arr[pointer]:
+            arr.pop(pointer)
+            arr.pop(0)
+            pointer = 1
+        else:
+            pointer += 1
+    return arr[0]
 
-    pass
 
 
 if __name__ == '__main__':
