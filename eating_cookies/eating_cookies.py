50a8f3ad15f5dce34a33fceca9ce26b65cc2ca19
Claudia Maciel <clc_80@yahoo.com>
2020-10-06 20:12:46 -0700

    eating_cookies complete

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,8 +4,17 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+    # base cases for  0 to 3 cookies
+    if n == 0 or n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
 
-    pass
+    # run through itself as many times as we can get the number of cookies to subtract by 1, 2, & 3
+    return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+    
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
