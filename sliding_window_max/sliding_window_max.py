2f1f7e22c59ecf0245761a38549a6a79f24ba91b
Claudia Maciel <clc_80@yahoo.com>
2020-10-06 19:21:07 -0700

    sliding_window_max complete

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,8 +4,25 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    # start at the first element
+    first_element = 0
+    # the last element is always "k" away from the first element
+    last_element = first_element + k
+    # create a new array to store the new items
+    new_array = []
+    
+    # loop until the last element is no longer on the array
+    while last_element <= len(nums):
+        # get the max number from the temp array
+        new_item = max(nums[first_element:last_element])
+        # add this new max number to the new array we created
+        new_array.append(new_item)
+        
+        # move on to the next element
+        first_element += 1
+        last_element = first_element + k
 
-    pass
+    return new_array
 
 
 if __name__ == '__main__':
