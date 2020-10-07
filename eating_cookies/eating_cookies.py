e993284db8e36a81f800b5030e28b2f694fc9e35
jarparrish <jarparrish@gmail.com>
2020-10-06 21:13:11 -0700

    all but cookies done - cookies close

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,49 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+import sys
 
-    pass
+def eating_cookies(n):  
+    possibilities = 0
+    ways_to_eat = 3
 
-if __name__ == "__main__":
+    if n <= -1:
+        return
+
+    elif n == 0:
+        possibilities = 1
+
+    elif n == 1:
+        possibilities = 1
+
+    else: 
+        def helper(n, currChoices):
+            # base case - num_cookies == 0
+            if n == 0:
+                nonlocal possibilities
+                possibilities += 1
+                return
+
+            for way in range(ways_to_eat):
+            
+                # make case if next possibility will make cookie monster eat too many cookies
+                if currChoices + (way + 1) < -1:
+                    return
+                else:
+                    helper(0, currChoices + (way + 1))
+        
+            return possibilities
+
+        helper(n, 0)
+
+    return possibilities
+       
+
+print(eating_cookies(2))
+
+
+""" if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies") """
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,9 +3,32 @@
 import sys
 
 def rock_paper_scissors(n):
-  # Your code here
+  # Create empty list for all possible plays
+  possible_plays = []
 
-  pass
+  # create list of different choices player can make
+  choices = ['rock', 'paper', 'scissors']
+
+  #create helper function
+  def helper(n, currChoices):
+    #create base case - base case will be when n == 0 for each instance of helper
+    if n == 0:
+      #before returning, append the new choice cobination to the possible plays list 
+      possible_plays.append(currChoices)
+      return
+    # implement recursion. We'll create a loop that inititializes instances of the helper function
+    # Within each loop, we'll initialize an instance of the helper function n times, decrementing n on each loop. 
+    # Note that each instance of Helper will start another loop / round of helper functions until each is resolved by reaching n == 0
+    # Each time a new helper is initialized, it is initialized with a new / additional choice added through the currChoice param
+    
+
+    for choice in range(len(choices)):
+      list_choice = [choices[choice]]
+      helper(n - 1, currChoices + list_choice)
+  helper(n, [])
+  return possible_plays
+
+
 
 
 if __name__ == "__main__":
