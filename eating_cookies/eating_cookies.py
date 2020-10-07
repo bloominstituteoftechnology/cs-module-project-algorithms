a631df13b0b500736a82da46f37982688d06c1cf
seanacres <6416690+seanacres@users.noreply.github.com>
2020-10-07 15:53:04 -0400

    Solution that works for small n

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,16 @@ Returns: an integer
 def eating_cookies(n):
     # Your code here
 
-    pass
+    #base case
+    if n == 0 or n ==1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+
+    #use recursion by subtracting the possible number of cookies eaten
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
