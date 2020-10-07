40d630c2764b1569c7aff271cbcc725ae5e23f1d
Alex Thompson <guitarman226@gmail.com>
2020-10-06 22:27:40 -0500

    Implemented eating cookies function

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,8 +2,26 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache):
     # Your code here
+    if n < 0:
+        return 0
+    
+    if n == 0:
+        cache[0] = 1
+        return 1
+    
+    if n in cache:
+        return cache[n]
+    
+    result_n_1 = eating_cookies(n-1, cache)
+    result_n_2 = eating_cookies(n-2, cache)
+    result_n_3 = eating_cookies(n-3, cache)
+    result_at_n = result_n_1 + result_n_2 + result_n_3
+    
+    cache[n] = result_at_n
+    
+    return result_at_n
 
     pass
 
@@ -11,4 +29,4 @@ if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies, {})} ways for Cookie Monster to each {num_cookies} cookies")
