265233dc4707c5f3c7a7c3941e4be7490cd72067
Neha-Kumari31 <Neha-kumari31@user.noreply.github.com>
2020-10-07 23:35:03 -0400

    Finished MVP

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,48 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+# def eating_cookies(n):
+#     # 3 choices to eat cookies
+#     # there is only one way to eat one cookie
+#     if n < 0:
+#         return 0
+#     if n==0 :
+#         return 1
+#     else:
+#        return  eating_cookies(n-1) + eating_cookies(n-2)+ eating_cookies(n-3)
+# #there is a lot of redundant work in first solution
+# use datastructure to cache(or save) the answer that has already calculated 
+# by other branches
+#what datastructure should we use for the cache?
+#save the n value and answer associated with the n value
+#we need to pass the cache the dictionary to the recursive call
+# to access as well as update the cache
 
-    pass
+#with cache every n value computed once
+# the cache reduce the runtime from O( 3^n) to O(n)
+
+def eating_cookies(n, cache):
+    # 3 choices to eat cookies
+    # there is only one way to eat one cookie
+    if n < 0:
+        return 0
+    if n==0 :
+        return 1
+        #check the cache to see if it holds the answer for the branch
+    elif n  in cache:
+        return cache[n]
+
+    else:
+       #otherwise, n is not in the cache, so we need to calculate the anwser for the branch
+       #save the answer in the cache
+       cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache)+ eating_cookies(n-3, cache)
+       return cache[n]
+
+
+
+
+
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,13 +3,71 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    '''
+    list ->int
+    Return odd-number-out element
+    '''
+    # # check every element appears once or not
+    for i in range(0, len(arr)):
+        j=i+1
+        #keep comparing i th  element with the others element
+        # until we find match
+        count = ''
+        while count !='found' and j < len(arr)-1:
+             if arr[i] == arr[j]:
+                 count = 'found'  
+                 #pop the value from the array
+                 arr.pop(j)
+             else: 
+                 j=j+1
+        # Return the number if we did not find the match
+        if count !='found':
+             return arr[i]
+'''
+ Better soluntion with singlr loop but still O(n^2)
+ count function has O(n) runtime
+ Better solution  in reading perspective
+ Is there datastructure that  would help us
+ Dictionary  and setsis a good datastructure in finding the duplicate values
+ Space Complexity - addition datastructure or varibale used in the program to store that
+'''
+    
+def single_number(arr):
+    '''
+    list ->int
+    Return odd-number-out element
+    '''
+    for x in arr:
+        if arr.count(x) ==1:
+            return x
 
-    pass
+def single_number(arr):
+    '''
+    list ->int
+    Return odd-number-out element
+    '''
+    # as far as the space complexity concerned
+    #worst case would be we have half of the element store
+    #in the sets before we start removing it
+    #initialize a sets-O(1)
+    s = set() 
+    #loop through every element in the list
+    for i in arr: #O(n)
+        if i in s: #O(1)
+            s.remove(x) # O(1)
+        else:
+            s.add(x) # O(1)
+    return list(s)[0] # O(1)
 
 
+ 
+        
+
+        
+            
+                       
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    arr = [1, 1, 5, 3,5, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
