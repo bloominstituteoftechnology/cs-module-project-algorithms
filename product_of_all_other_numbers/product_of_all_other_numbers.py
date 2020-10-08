e47b3a072bafdf59c3b4cd242cd2b8871770144c
Matt Martindale <martindalematt18@yahoo.com>
2020-10-08 14:30:45 -0600

    Update product_of_all_other_numbers.py

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,23 +3,30 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    index = 0
+  product_array = []
+  index = 0
 
-    for num in arr:
-        arr[index] = 1
+  while index < len(arr):
 
-        arr[index] = [num * num in arr]
+    product = 1
 
-        index += 1
+    for num in range(0, len(arr)):
+      if index == num:
+        continue
+      else:
+        product *= arr[num]
+
+    product_array.append(product)
+    index += 1
     
-    return arr
+  return product_array
 
 
 
 
 if __name__ == '__main__':
-    # Use the main function to test your implementation
-    arr = [1, 2, 3, 4, 5]
-    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+  # Use the main function to test your implementation
+  arr = [1, 2, 3, 4, 5]
+  # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+  print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
