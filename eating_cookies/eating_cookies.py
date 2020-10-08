d4243bc2f38bbbe03245b74dd39a4f019229e434
FabiolaSaga <saga.fabiola@gmail.com>
2020-10-08 18:15:18 -0500

    Finished mvp

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,25 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache = None):
     # Your code here
+    if n == 0 or n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+    if cache is None:
+       cache = {}
+    
+    if cache and cache[n]:
+        return cache[n]
+    
+    cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    return cache[n]
+    
 
-    pass
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
