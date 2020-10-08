8de56835b4c442b52fbeaec93c2c14e755c4f49d
hai666l <0xconr@protonmail.com>
2020-10-08 18:50:18 -0500

    eating_cookies.py with tests passing

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,14 +1,5 @@
-'''
-Input: an integer
-Returns: an integer
-'''
 def eating_cookies(n):
-    # Your code here
-
-    pass
-
-if __name__ == "__main__":
-    # Use the main function here to test out your implementation
-    num_cookies = 5
-
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+	s = [1, 2, 4]
+	for i in range(0, n):
+		s = [s[1], s[2], sum(s)]
+	return s[2] - sum(s[:2])
