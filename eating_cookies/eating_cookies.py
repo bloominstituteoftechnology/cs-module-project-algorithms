5cce5aa12d87ad8a4a95962afee04bff0e896527
Alex Thompson <guitarman226@gmail.com>
2020-10-08 21:13:36 -0500

    Spelling error that bothered me

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -29,4 +29,4 @@ if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies, {})} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies, {})} ways for Cookie Monster to eat {num_cookies} cookies")
