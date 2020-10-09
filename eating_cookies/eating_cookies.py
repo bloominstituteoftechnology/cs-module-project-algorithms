12c06ca8289356b88c82cab75df05b784dbd54e9
rfamilara <techknohauz.com>
2020-10-08 20:32:25 -0500

    Code product_of_all_numbers Completed Tests were succesful

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,7 +4,6 @@ Returns: an integer
 '''
 def eating_cookies(n, cache = None):
     # Your code here
-
     if n < 0:
         return 0
     if n == 0:
@@ -13,7 +12,7 @@ def eating_cookies(n, cache = None):
         cache = {}
     if n in cache:
         return cache[n]
-
+    
     cache[n] = eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
     return cache[n]
 
@@ -22,3 +21,4 @@ if __name__ == "__main__":
     num_cookies = 5
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+
--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -8,9 +8,6 @@ Item = namedtuple('Item', ['index', 'size', 'value'])
 def knapsack_solver(items, capacity):
     # Your code here
 
-    pass
-
-
 if __name__ == '__main__':
   if len(sys.argv) > 1:
     capacity = int(sys.argv[2])
