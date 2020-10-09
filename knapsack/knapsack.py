11527237ae50939df95a63121e6796b1d7aa5f1b
rfamilara <techknohauz.com>
2020-10-08 21:30:24 -0500

    Code single_number.py -Completed -tests were successful

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -8,6 +8,9 @@ Item = namedtuple('Item', ['index', 'size', 'value'])
 def knapsack_solver(items, capacity):
     # Your code here
 
+    pass
+
+
 if __name__ == '__main__':
   if len(sys.argv) > 1:
     capacity = int(sys.argv[2])
