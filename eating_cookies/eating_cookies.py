c06edbbbc4cddc2fc5bf329a7334213083028155
Adam-Young <awy6448@gmail.com>
2020-10-08 23:29:44 -0400

    Optimized eating cookies, producct of all and sliding windows.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,55 +2,17 @@
 Input: an integer
 Returns: an integer
 '''
-from itertools import permutations 
-def eating_cookies(n):
-    # cookies = n
-    # three = []
-    # two = []
-    # if cookies == 0:
-    #   return 1
-    # if cookies == 1:
-    #   return 1
-    # if cookies == 2:
-    #   return 2
-    # while cookies != 0:
-    #   if cookies >= 3:
-    #     cookies = cookies -3
-    #     three.append(3)
-    #   elif cookies == 2:
-    #     cookies = cookies -2
-    #     three.append(2)
-    #   else:
-    #     cookies = cookies -1
-    #     three.append(1)
-    # cookies = n
-    # while cookies != 0:
-    #   if cookies >= 2:
-    #     cookies = cookies -2
-    #     two.append(2)
-    #   else:
-    #     cookies = cookies -1
-    #     two.append(1)
-    # three = len(set(permutations(three)))
-    # two = len(set(permutations(two)))
-    # one = 1
-    # total = three + two + one
-    # print(total)
-    # print(set(permutations([3,3,3,1])))
-    # return total
-    cookies = n
-    total = 0
-    if cookies > 3:
-      total += eating_cookies(n-3)
-      total += eating_cookies(n-2)
-      total += eating_cookies(n-1)
-      return total
-    elif cookies == 3:
-      return 4
-    elif cookies ==2:
-      return 2
-    else:
-      return 1
+def eating_cookies(n, cache):
+    if n == 0: 
+      cache[n] = 1
+    if n == 1 or n == 2: 
+      cache[n] = n
+
+
+    if cache[n] == 0:
+      cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
+
 
 
 
@@ -59,4 +21,4 @@ if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies, [0 for i in range(6)])} ways for Cookie Monster to each {num_cookies} cookies")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,13 +2,29 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+from collections import deque
+
 def sliding_window_max(nums, k):
-    win = []
+    # win = []
     top = []
-    for i in range(len(nums) - k + 1):
-      win = nums[i:i+k]
-      top.append(max(win))
-    return top 
+    # for i in range(len(nums) - k + 1):
+    #   win = nums[i:i+k]
+    #   top.append(max(win))
+    # return top 
+    que = deque()
+    for i in range(k):
+      while que and nums[i] >= nums[que[-1]]:
+        que.pop()
+      que.append(i)
+    for i in range(k, len(nums)):
+      top.append(nums[que[0]])
+      while que and que[0] <= i-k:
+        que.popleft()
+      while que and nums[i] >= nums[que[-1]]:
+        que.pop()
+      que.append(i)
+    top.append(nums[que[0]])
+    return top
 
 
 
