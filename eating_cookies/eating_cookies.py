0463895e7d0720d0f4c40cf6675388cecffc5d67
Veto Ramirez <vetorobert3@gmail.com>
2020-10-08 20:31:26 -0700

    finished the 4 files

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,15 +4,14 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
-    eats = -1
+    # eats = -1
 
     if n < 0:
-        eats = 0
+        return 0
     elif n == 0:
-        eats=1
+        return 1
     else:
-        eats = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
-    return eats
+        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
