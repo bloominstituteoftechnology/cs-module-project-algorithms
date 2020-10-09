02260deba4a655e91d5f9460671cd96a9e422c06
kmk028 <kmk028@users.noreply.github.com>
2020-10-08 19:06:12 -0700

    moving zeros done

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,28 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    #right and left pointers 
+    i = 0
+    j = 1
+    
+    while j<len(arr):
+        # if both are both then only move right pointer 
+        if (arr[i] == 0 and  arr[j] == 0):
+            j +=1
+        #if left pointer is 0, right is non zero swap them and move both forward. 
+        elif (arr[i] == 0 and arr[j] !=0):
+            arr[i],arr[j] = arr[j],arr[i]
+            j+=1
+            i+=1
+         #if left pointer is not 0, right is non zero or zero do nothing, move forward.
+        elif (arr[i] != 0 and arr[j] ==0):
+            j+=1
+            i+=1
+        else:
+            j+=1
+            i+=1
+        
+    return arr
 
 
 if __name__ == '__main__':
