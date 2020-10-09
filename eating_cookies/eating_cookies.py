e690d482b774e896a7cd12f2af20acae369478ce
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2020-10-09 12:43:31 +0900

    Complete cookie eating monster with first pass solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,9 +3,36 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
+    if n == 0:
+        return 1
     # Your code here
+    counter = 0
+    option_list = [1,2,3]
 
-    pass
+    def rec(num_cookies_left, *args):
+        if num_cookies_left == 0:
+            print('NO COOKIES LEFT! EXIT')
+            return
+        print('args', args)
+        if args:
+            option = args[0]
+            if num_cookies_left >= option:
+                print(f'we can still eat!! num cookie left: {num_cookies_left} with option: {option}')
+                num_cookies_left -= option
+                if num_cookies_left == 0:
+                    nonlocal counter
+                    counter += 1
+                    print(f'counter ++ {counter}')
+            else:
+                return
+            
+        for item in option_list:
+            print(f'lets go recursion with item: {item}')
+            rec(num_cookies_left, item)    
+    
+    rec(n)
+    print('counter', counter)
+    return counter
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
