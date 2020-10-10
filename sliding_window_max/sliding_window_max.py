c11f06bc17471817db481f62288d0cb6fdc1966f
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-10-10 11:11:17 -0700

    mvp completed

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,17 +2,36 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+from collections import deque
+
 def sliding_window_max(nums, k):
     # Your code here
 
-    left_index = 0
-    right_index = 2
-    maxx = []
+    max_vals = []
+    q = deque()
+    # remove all elems from a queue
+    for i, n in enumerate(nums):
+        while len(q) > 0 and n > q[-1]:
+            q.pop()
+        
+        q.append(n)
 
-    while nums[left_index] == 0 and nums[right_index] == 2:s
-        maxx.append(max(nums[left_index : right_index]))
-        left_index += 1
-        right_index += 1
+        # calc the window range
+        window_range = i - k + 1
+
+        # as long as our windows range == k, then we will add elements to the queue
+        if window_range >= 0:
+            # add the max elem (in this case 1st in the queue) to the max_vals
+            max_vals.append(q[0])
+
+            # check num on the left needs to be evicted
+            # if so take it out of the start of the queue
+            if nums[window_range] == q[0]:
+                q.popleft()
+
+    return max_vals
+    
 
     
 
