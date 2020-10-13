a6ad238b11842c70d1da3374e722c0fdec492bd6
rfamilara <techknohauz.com>
2020-10-12 21:38:05 -0500

    Completed sliding_window_max -Tests successful

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,23 +2,21 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n, cache = None):
-    # Your code here
-    if n < 0:
-        return 0
-    if n == 0:
+
+def eating_cookies(n):
+    if n == 0 or n == 1:
         return 1
-    if cache is None:
-        cache = {}
-    if n in cache:
-        return cache[n]
-    
-    cache[n] = eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
-    return cache[n]
+
+    if n == 2:
+        return 2
+
+    if n == 3:
+        return 4
+
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
-
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -5,22 +5,26 @@ Returns: a List of integers
 def sliding_window_max(nums, k):
     # Your code here
     
-    start = 0
-    end = len(nums)
+    m = max(nums[:k])
+    result = [m]
 
-    max_nums = []
-    for i in range(end - k + 1):
-        start = nums[i]
+    for i in range(1, len(nums)-k+1):
+        if nums[i+k-1] > m:
+            m = nums[i+k-1]
+        elif nums[i-1] == m:
+            m = max(nums[i:i+k])
+        result.append(m)
+    return result
 
-        for num in range(1, k):
 
-            if nums[i + num] > start:
-                start = nums[i + num]
 
-        max_nums.append(start)
-        
-    return max_nums
 
+if __name__ == '__main__':
+    # Use the main function here to test out your implementation
+    arr = [1, 3, -1, -3, 5, 3, 6, 7]
+    k = 3
+
+    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
 
 
 if __name__ == '__main__':
