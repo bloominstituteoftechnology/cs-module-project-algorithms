0d48aba917a410e0d727661e9bcff5cfed7d6a7d
Alex Thompson <guitarman226@gmail.com>
2020-10-12 19:14:54 -0500

    Implemented rps functionality

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,9 +3,29 @@
 import sys
 
 def rock_paper_scissors(n):
-  # Your code here
+  return combinations(n, [])
+  
+def combinations(n, current_list):
+  choices = ["rock", "paper", "scissors"]
+  
+  if len(current_list) == 0:
+    if n < 1:
+      return [[]]
+    return combinations(n - 1, [[c] for c in choices])
+  
+  if n < 1:
+    return current_list
+  
+  new_list = []
+  for combo in current_list:
+    for choice in choices:
+      new_combo = combo.copy()
+      new_combo.append(choice)
+      new_list.append(new_combo)
+      
+  return combinations(n - 1, new_list)
 
-  pass
+
 
 
 if __name__ == "__main__":
